482c8f3bc34efbc6e6a30c41624fb2bef6368698
coramdeo643 <coramdeo643@kakao.com>
2025-07-02 10:25:27 +0900

    Reply/Reply JPA Repository

--- src/main/java/com/tenco/blog/board/Board.java
+++ src/main/java/com/tenco/blog/board/Board.java
@@ -1,6 +1,7 @@
 package com.tenco.blog.board;
 
 
+import com.tenco.blog.reply.Reply;
 import com.tenco.blog.user.User;
 import com.tenco.blog.utils.MyDateUtil;
 import jakarta.persistence.*;
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -47,5 +50,21 @@ public class Board {
         return MyDateUtil.timestampFormat(createdAt);
     }
 
+    /*
+      Board와 Reply를 양방향 매핑 설계해보자
+      하나의 게시글(one)에는 여러개의 댓글(many)을 가질수있다
+
+      테이블 기준으로 고민을 해본다면 게시글 테이블과 댓글 테이블 관계를 형성할때,
+      FK는 누가 들고있어야하나? Reply,
+      연관관계의 주인은 Reply(FK)
+      mappedBy = FK 주인이 아닌 Entity에 설정해야한다
+      cascade = CascadeType.REMOVE
+       = 영속성 전이 : 게시글 삭제 시, 관련된 모든 댓글도 자동 삭제 처리한다
+        = 데이터 무결성 보장하기 위해서
+
+     */
+    @OrderBy("id desc") // 정렬 옵션 설정 (내림차순)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "board", cascade = CascadeType.REMOVE)
+    List<Reply> replies = new ArrayList<>(); // List 선언과 동시에 초기화
 
 }
